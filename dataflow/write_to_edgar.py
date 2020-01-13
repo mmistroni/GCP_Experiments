ba0db04c948b1a5188f6012ba1a8d677170663d0
mmistroni <mmistroni@gmail.com>
2020-01-13 22:21:34 +0000

    Adding extra params

--- dataflow/write_to_edgar.py
+++ dataflow/write_to_edgar.py
@@ -27,6 +27,32 @@ filename = '{}_{}'.format(form_type, datetime.now().strftime('%Y$m%d-%H%M'))
 ### BIG QUERY CONFIGS
 ## BIG QUERY SCHEMA
 
+
+class MyOptions(PipelineOptions):
+  @classmethod
+  def _add_argparse_args(cls, parser):
+      parser.add_value_provider_argument('--year',
+                                         dest='input',
+                                         default='2019',
+                                         help='Input year to process')
+      parser.add_value_provider_argument('--output',
+                                         dest='output',
+                                         default='gs://mm_dataflow_bucket/outputs',
+                                         help='Input year to process')
+
+      parser.add_value_provider_argument(
+          '--runner',
+          default='DataFlowRunner',
+          help='Runner to use')
+      parser.add_value_provider_argument(
+          '--template_location',
+          help='Template Location')
+      parser.add_value_provider_argument(
+          '--staging_location',
+          default='gs: //mm_dataflow_bucket/staging',
+          help='Staging Location')
+
+
 def get_edgar_table_schema():
   edgar_table_schema = 'COB:STRING, CUSIP:STRING, COUNT:INTEGER, TICKER:STRING'
   return edgar_table_schema
@@ -39,31 +65,14 @@ def get_edgar_table_spec():
 
 def run(argv=None, save_main_session=True):
   parser = argparse.ArgumentParser()
-  parser.add_value_provider_argument('--year',
-                      dest='input',
-                      default='2019',
-                      help='Input year to process')
-  parser.add_value_provider_argument('--output',
-                      dest='output',
-                      default='gs://mm_dataflow_bucket/outputs',
-                      help='Input year to process')
-
-  parser.add_value_provider_argument(
-      '--runner',
-      default='DataFlowRunner',
-      help='Runner to use')
-  parser.add_value_provider_argument(
-      '--template_location',
-      help='Template Location')
-  parser.add_value_provider_argument(
-      '--staging_location',
-      default='gs: //mm_dataflow_bucket/staging',
-      help='Staging Location')
 
   known_args, pipeline_args = parser.parse_known_args(argv)
 
   pipeline_options = PipelineOptions(pipeline_args)
-  pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+
+  #pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+
+  pipeline_options.view_as(MyOptions).save_main_session = save_main_session
 
   p4 = beam.Pipeline()
 
