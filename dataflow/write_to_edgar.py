ffd5ce1656a47bba0523a7710c7fcdaf3362bf0b
mmistroni <mmistroni@gmail.com>
2020-01-14 21:19:12 +0000

    Amending pipeline params

--- dataflow/write_to_edgar.py
+++ dataflow/write_to_edgar.py
@@ -76,11 +76,11 @@ def run(argv=None, save_main_session=True):
   #known_args, pipeline_args = parser.parse_known_args(argv)
 
   pipeline_args = []
-  pipeline_args.append('--project={}'.format(GC_PROJECT))
-  pipeline_args.append('--runner={}'.format(RUNNER))
-  pipeline_args.append('--staging_location={}'.format(STAGING_BUCKET))
-  pipeline_args.append('--temp_location={}'.format(TEMP_BUCKET))
-  pipeline_args.append('--template_location={}'.format(TEMPLATE_BUCKET))
+  pipeline_args.append('--project {}'.format(GC_PROJECT))
+  pipeline_args.append('--runner {}'.format(RUNNER))
+  pipeline_args.append('--staging_location {}'.format(STAGING_BUCKET))
+  pipeline_args.append('--temp_location {}'.format(TEMP_BUCKET))
+  pipeline_args.append('--template_location {}'.format(TEMPLATE_BUCKET))
 
   pipeline_options = PipelineOptions(pipeline_args)
 
