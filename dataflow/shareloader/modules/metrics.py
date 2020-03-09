ab13eadc83ce05feb58dbc2c58bfe66395d4f76d
mmistroni <mmistroni@gmail.com>
2020-03-09 21:41:24 +0000

    Fixed minor issues with packages

---
+++ dataflow/shareloader/modules/metrics.py
@@ -0,0 +1,62 @@
+
+
+import logging
+logger = logging.getLogger(__name__)
+from math import sqrt
+from pprint import pprint
+from datetime import date
+
+def calculate_daily_returns(prices):
+  return prices.pct_change(1)
+
+def calculate_daily_cumulative_returns(daily_pc):
+  return (1 + daily_pc).cumprod()
+
+def compute_standard_deviation(daily):
+  return daily.loc[:,daily.columns[0]].std()
+
+def compute_sharpe_ratio(s_prices):
+  # This function should be used in final dataframe
+  # USE THSI FUNCTION
+  dret = calculate_daily_returns(s_prices)
+  avg = dret.loc[:,dret.columns[0]].mean()
+  std = compute_standard_deviation(dret)
+  return (sqrt(252) * avg) / std
+
+def compute_moving_averages(prices, day):
+  return prices.rolling(window=day).mean()
+
+def check_prices_vs_moving_averages(prices, day=30):
+  # This Function should be used  in final dataframe
+  ma30 = compute_moving_averages(prices, 30)
+  ticker_col = ma30.columns[0]
+  m30_col = '{}M30'.format(ticker_col)
+  ma30_renamed = ma30.rename({ticker_col: m30_col}, axis=1)
+  concats = pd.concat([prices, ma30_renamed], axis=1)
+  concats['AboveM30'] = concats[ticker_col] > concats[m30_col]
+  above_m30 = concats[concats['AboveM30'] == True]
+  total_prices = prices.shape[0]
+  total_m30 = above_m30.shape[0]
+  pcnt = 1.0*total_m30/total_prices
+  return pcnt
+
+def compute_data_performance(historical_df, ticker):
+  # Use this FUNCTION CALL
+  start = historical_df[ticker].values[0]
+  end = historical_df[ticker].values[-1]
+  return end*1.0/start - 1
+
+def compute_metrics(prices):
+  # Add Trade Volumne to spot momentum stocks
+  ticker = prices.columns[0]
+  perf_dict = {}
+  perf_dict['Ticker'] = ticker
+  perf_dict['Performance'] = compute_data_performance(prices, ticker)
+  perf_dict['Start_Price'] = prices[ticker].values[0]
+  perf_dict['End_Price'] = prices[ticker].values[-1]
+  perf_dict['AboveMovingAvgPcnt'] = check_prices_vs_moving_averages(prices)
+  perf_dict['SharpeRatio'] = compute_sharpe_ratio(prices)
+  #news_dict =calculate_news_sentiment(ticker)
+  #news_measure = sum(news_dict['positive']) + sum(news_dict['negative'])
+  #perf_dict['News_Sentiment'] = news_measure
+  return pd.DataFrame([perf_dict])
--- dataflow/shareloader/modules/shareloader.py
+++ dataflow/shareloader/modules/shareloader.py
@@ -19,18 +19,33 @@ import requests
 import os
 import pandas_datareader.data as dr
 from datetime import date
+from .metrics import compute_metrics
+from pandas.tseries.offsets import BDay
+import logging
+logger = logging.getLogger().setLevel(logging.INFO)
+
+
+class XyzOptions(PipelineOptions):
+
+  @classmethod
+  def _add_argparse_args(cls, parser):
+    parser.add_argument('--business_days', default=60)
+
 
 def get_tickers():
     return['AAPL', 'AMZN', 'MSFT']
 
-def get_latest_price_yahoo(symbol):
+def get_latest_price_yahoo(symbol, business_days):
   try:#
-    as_of_date = date(2020,2,28)
-    print('--latest price for{}'.format(symbol))
-    res = dr.get_data_yahoo(symbol, as_of_date, as_of_date)[['Close']]
-    df['Symbol'] = symbol
-    return df
+    logger.info('Fetching  prices for last :{}'.format(business_days))
+    as_of_date = datee.today()
+    start_date = as_of_date - BDay(60)
+    logger.info('--latest price for{}'.format(symbol))
+    res = dr.get_data_yahoo(symbol, start_date, as_of_date)[['Adj Close', 'Volume']]
+    res['Symbol'] = symbol
+    return res
   except Exception as e :
+    logger.info('Exception in loading latest prices:{}'.format(str(e)))
     return pd.DataFrame(columns=[symbol])
 
 
@@ -41,14 +56,18 @@ def run(argv=None, save_main_session=True):
 
   known_args, pipeline_args = parser.parse_known_args(argv)
 
+  pipeline_options = XyzOptions()
+  pipeline_options.view_as(SetupOptions).save_main_session  = save_main_session
+  print('Options are:{}'.format(pipeline_options.get_all_options()))
+  print('Busdays:{}'.format(pipeline_options.business_days))
 
-  p4 = beam.Pipeline(options=PipelineOptions())
+  p4 = beam.Pipeline(options=pipeline_options)
 
   lines = (
        p4
        #| 'generate master url' >>beam.Create(['https://www.sec.gov/Archives/edgar/full-index/2019/QTR1/master.idx'])
        | 'Sampling Data' >> beam.Create(get_tickers())
-       | 'Getting Latest Prices' >> beam.Map(lambda symbol: get_latest_price_yahoo(symbol))
+       | 'Getting Latest Prices' >> beam.Map(lambda symbol: get_latest_price_yahoo(symbol, pipeline_options.business_days))
        | 'Printing Out Results' >> beam.Map(print)
 
   )
