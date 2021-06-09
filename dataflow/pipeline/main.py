adf6b3cdeb3c938eb37dc6c24825b8c9dd5b5c8b
mmistroni <mmistroni@gmail.com>
2021-06-09 20:58:49 +0100

    Committing what we got

--- dataflow/pipeline/main.py
+++ dataflow/pipeline/main.py
@@ -1,5 +1,6 @@
-from .edgar_quarterly_form4 import run
 import logging
+import modules.edgar_quarterly_form4 as ef4
+
 if __name__ == '__main__':
-  logging.getLogger().setLevel(logging.INFO)
-  run()
+    logging.getLogger().setLevel(logging.INFO)
+    ef4.run()
