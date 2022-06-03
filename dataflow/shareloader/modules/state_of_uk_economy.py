6bf0920a8988c9ae352dbf92e9a40d9944e83d18
mmistroni <mmistroni@gmail.com>
2022-06-03 09:52:51 +0100

    adding uk stats

--- dataflow/shareloader/modules/state_of_uk_economy.py
+++ dataflow/shareloader/modules/state_of_uk_economy.py
@@ -21,6 +21,7 @@ from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 import re, requests
 from datetime import datetime, date
+import pandas as pd
 from collections import OrderedDict
 import requests
 from sendgrid import SendGridAPIClient
@@ -34,31 +35,66 @@ from .mail_utils import STOCK_EMAIL_TEMPLATE
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, Email, Personalization
 
-ROW_TEMPLATE =  """<tr><td>{}</td>
-                       <td>{}</td>
-                       <td>{}</td>
-                       <td>{}</td>
-                       <td>{}</td>
-                       <td>{}</td>
-                       <td>{}</td>
-                       <td>{}</td>
-                       <td>{}</td>
-                       <td>{}</td>
-                       <td>{}</td>
-                       </tr>"""
+
+import csv, urllib.request
+import io
+from urllib.request import Request, urlopen
+import csv, requests
+import codecs
+from bs4 import BeautifulSoup
+import requests
+
+def fetchFruitAndVegPrices():
+    baseUrl = 'https://www.gov.uk/government/statistical-data-sets/wholesale-fruit-and-vegetable-prices-weekly-average'
+    req = requests.get(baseUrl)
+    soup = BeautifulSoup(req.text, "html.parser")
+    span = soup.find_all('span', {"class": "download"})[0]
+    anchor = span.find_all('a', {"class": "govuk-link"})[0]
+    link = anchor.get('href')
+    r = requests.get(link).text
+    dt = pd.read_csv(io.StringIO(r), header=0)
+    dt['asOfDate'] = pd.to_datetime(dt['date'], infer_datetime_format=True)
+    return dt[dt.asOfDate == dt.asOfDate.max()]
+
+def get_petrol_prices():
+    url = 'https://www.gov.uk/government/statistics/weekly-road-fuel-prices'
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+    span = soup.find_all('span', {"class": "download"})[0]
+    anchor = span.find_all('a', {"class": "govuk-link"})[0]
+    link = anchor.get('href')
+    r = requests.get(link).text
+    dt = pd.read_csv(io.StringIO(r), header=2)[-1:][['Date','ULSP', 'ULSD']]
+    dt.set_index('Date', inplace=True)
+    return dt
+
+def get_latest_url():
+    url = "https://cy.ons.gov.uk/datasets/online-job-advert-estimates/editions"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+    anchor = soup.find_all('a', {"id": "edition-time-series"})[0]
+    suffix = anchor.get('href')
+    return f'https://download.ons.gov.uk/downloads{suffix}.csv'
 
 
-def create_monthly_data_ppln(p):
-    cutoff_date_str = (date.today() - BDay(60)).date().strftime('%Y-%m-%d')
-    logging.info('Cutoff is:{}'.format(cutoff_date_str))
-    bq_sql = """SELECT TICKER, COUNT(*) as COUNTER FROM `datascience-projects.gcp_shareloader.stock_selection` 
-        WHERE AS_OF_DATE > PARSE_DATE("%F", "{}") AND LABEL <> 'STOCK_UNIVERSE' GROUP BY TICKER 
-  """.format(cutoff_date_str)
-    logging.info('executing SQL :{}'.format(bq_sql))
-    return (p | 'Reading-{}'.format(cutoff_date_str) >> beam.io.Read(
-        beam.io.BigQuerySource(query=bq_sql, use_standard_sql=True))
+def get_latest_jobs_statistics():
+    latestUrl = get_latest_url()
+    print(f'Latest URL from ONS is {latestUrl}')
+    res = requests.get(latestUrl, headers={'User-Agent': 'Mozilla/5.0'})
+    # 'https://download.ons.gov.uk/downloads/datasets/online-job-advert-estimates/editions/time-series/versions/20.csv'
+    text = res.iter_lines()
+    data = csv.reader(codecs.iterdecode(text, 'utf-8'), delimiter=',')
+    headers = ['v4_1',	'Data Marking', 	'calendar-years',	'Time',	'uk-only',	'Geography', 'adzuna-jobs-category',	'AdzunaJobsCategory',	'week-number',	'Week']
+    dataset = [d for d in data]
+    jobs_dataset = pd.DataFrame(dataset, columns=headers)
+    valid = jobs_dataset[(jobs_dataset.Time == str(date.today().year)) & (jobs_dataset.AdzunaJobsCategory.str.contains('Computing'))]
+    valid[['wk', 'wkno']] = valid['week-number'].str.split(pat = '-', expand = True)
+    filtered = valid[valid.v4_1.str.contains('.')]
+    filtered['wkint'] = filtered.wkno.apply(lambda v: int(v))
+    return filtered.sort_values(by=['wkint'])
 
-            )
+
+
 
 
 
