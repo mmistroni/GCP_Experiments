232a76418397fcd255d6596d2411ca255bc9aaff
mmistroni <mmistroni@gmail.com>
2022-06-06 18:23:26 +0100

    extracting only info needed

--- dataflow/shareloader/modules/economic_utils.py
+++ dataflow/shareloader/modules/economic_utils.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.request import Request, urlopen
 from datetime import date
+from dateutil.relativedelta import relativedelta
 
 def get_fruit_and_veg_prices():
     baseUrl = 'https://www.gov.uk/government/statistical-data-sets/wholesale-fruit-and-vegetable-prices-weekly-average'
@@ -17,8 +18,11 @@ def get_fruit_and_veg_prices():
     link = anchor.get('href')
     r = requests.get(link).text
     dt = pd.read_csv(io.StringIO(r), header=0)
-    dt['asOfDate'] = pd.to_datetime(dt['date'], infer_datetime_format=True)
-    return dt[dt.asOfDate == dt.asOfDate.max()][['date', 'category', 'item', 'variety', 'price', 'unit']]
+    dt['asOfDate'] = pd.to_datetime(dt['date'], infer_datetime_format=True).date()
+    latest = dt[dt.asOfDate == dt.asOfDate.max()][['asOfDate', 'category', 'item', 'variety', 'price', 'unit']]
+    latest['label'] = latest.apply(lambda r: f"{r['category']}-{r['item']}-{r['variety']}({r['unit']})", axis=1)
+    latest['value'] = latest.price.apply(lambda valstr: float(valstr))
+    return latest[['asOfDate', 'label', 'value']].to_dict('records')
 
 def get_petrol_prices():
     url = 'https://www.gov.uk/government/statistics/weekly-road-fuel-prices'
@@ -28,17 +32,23 @@ def get_petrol_prices():
     anchor = span.find_all('a', {"class": "govuk-link"})[0]
     link = anchor.get('href')
     r = requests.get(link).text
-    dt = pd.read_csv(io.StringIO(r), header=2)[-1:][['Date','ULSP', 'ULSD']]
-    dt.set_index('Date', inplace=True)
-    return dt
+    dt = pd.read_csv(io.StringIO(r), header=2)[-1:][['Date','ULSP', 'ULSD']].to_dict('records')[-1]
+    return [
+                {'asOfDate' : dt['Date'],
+                 'label' : 'Petrol',
+                  'value': float(dt['ULSP'])},
+                {'asOfDate': dt['Date'],
+                 'label': 'Diesel',
+                 'value': float(dt['ULSD'])}
+                ]
 
-def get_latest_url():
-    url = "https://cy.ons.gov.uk/datasets/online-job-advert-estimates/editions"
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, "html.parser")
-    anchor = soup.find_all('a', {"id": "edition-time-series"})[0]
-    suffix = anchor.get('href')
-    return f'https://download.ons.gov.uk/downloads{suffix}.csv'
+    def get_latest_url():
+        url = "https://cy.ons.gov.uk/datasets/online-job-advert-estimates/editions"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, "html.parser")
+        anchor = soup.find_all('a', {"id": "edition-time-series"})[0]
+        suffix = anchor.get('href')
+        return f'https://download.ons.gov.uk/downloads{suffix}.csv'
 
 
 def get_latest_jobs_statistics():
@@ -56,4 +66,10 @@ def get_latest_jobs_statistics():
     filtered = valid[valid.v4_1.str.contains('.')]
     filtered['wkint'] = filtered.wkno.apply(lambda v: int(v))
     filtered = filtered.rename(columns={'v4_1': 'jobs'})[['calendar-years', 'wkint', 'adzuna-jobs-category', 'jobs']]
-    return filtered.sort_values(by=['wkint'])
+    latest = filtered.sort_values(by=['wkint']).to_dict('records')[-1]
+    year = int(latest['calendar_years'])
+    asOfDate = date(year, 1, 1) + relativedelta(weeks =+ int(latest['wkint']))
+    return [{'label' : latest['adzuna-job-category'],
+            'asOfDate' : asOfDate,
+            'value' : float(latest['jobs'])}]
+
