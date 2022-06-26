e228c77d8b078ffe712d099b471cd5e837781a44
mmistroni <mmistroni@gmail.com>
2022-06-26 10:09:09 +0100

    fixignbugs

--- dataflow/shareloader/modules/superperf_metrics.py
+++ dataflow/shareloader/modules/superperf_metrics.py
@@ -165,8 +165,11 @@ def get_analyst_estimates(ticker, key,  fundamental_dict):
     try:
         analyst_estimates = requests.get('https://financialmodelingprep.com/api/v3/analyst-estimates/{ticker}?apikey={key}'.format(ticker=ticker, key=key)).json()
         # achievable. we just need to sort the date
+        income_statement_date = fundamental_dict.get('income_statement_date', date.today())
         year = datetime.strptime(income_statement_date, '%Y-%m-%d').date().year
         logging.info('getting estimates for {}@{}'.format(ticker, income_statement_date))
+
+
         if analyst_estimates:
             estimateeps_next = [data for data in analyst_estimates if str(year+1) in data['date']]
             if estimateeps_next:
