994ed3fce2990b74af82f75d920502dd4067d9ac
mmistroni <mmistroni@gmail.com>
2023-02-11 12:36:24 +0000

    adding logging

--- dataflow/shareloader/modules/premarket_loader.py
+++ dataflow/shareloader/modules/premarket_loader.py
@@ -280,6 +280,8 @@ def run(argv=None, save_main_session=True):
                 data = extract_trend_pipeline(p, pipeline_options.fmprepkey, pipeline_options.numdays)
                 destination = 'gs://mm_dataflow_bucket/inputs/historical_prices_5y_{}'.format(
                                         date.today().strftime('%Y-%m-%d %H:%M'))
+
+                logging.info(f'Writing to {destination}')
                 test_sink = beam.io.WriteToText(destination, num_shards=1)
 
             else:
