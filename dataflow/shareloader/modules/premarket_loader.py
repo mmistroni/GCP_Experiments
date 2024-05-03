c07b6863edc1d4d039a85621a1877b1317fa5fa4
mmistroni <mmistroni@gmail.com>
2024-05-03 07:47:50 +0100

    replaced output bucket

--- dataflow/shareloader/modules/premarket_loader.py
+++ dataflow/shareloader/modules/premarket_loader.py
@@ -472,7 +472,7 @@ def run(argv=None, save_main_session=True):
             if 'historical' in pipeline_options.mmrun:
                 logging.info('Running historical ppln..')
                 data = extract_trend_pipeline(p, pipeline_options.fmprepkey, pipeline_options.numdays)
-                destination = 'gs://mm_dataflow_bucket/dataset/supertrend_{}'.format(
+                destination = 'gs://mm_dataflow_bucket/datasets/supertrend_{}'.format(
                                         date.today().strftime('%Y-%m-%d %H:%M'))
 
                 logging.info(f'Writing to {destination}')
@@ -492,7 +492,7 @@ def run(argv=None, save_main_session=True):
 
 
                 data = extract_full_run_pipeline(p, pipeline_options.fmprepkey, all_tickers_5y, pipeline_options.numdays, )
-                destination = 'gs://mm_dataflow_bucket/inputs/historical_prices_5y_{}'.format(
+                destination = 'gs://mm_dataflow_bucket/datasets/historical_prices_5y_{}'.format(
                     date.today().strftime('%Y-%m-%d %H:%M'))
 
                 logging.info(f'Writing to {destination}')
