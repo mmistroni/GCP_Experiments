79b2b6a6bea3202cb2e8a0967e0b3bbf75442c6d
mmistroni <mmistroni@gmail.com>
2024-06-11 20:24:39 +0100

    Update setup_dftester.py

--- dataflow/shareloader/setup_dftester.py
+++ dataflow/shareloader/setup_dftester.py
@@ -1,1 +1,29 @@
 
+import setuptools
+
+# Configure the required packages and scripts to install.
+# Note that the Python Dataflow containers come with numpy already installed
+# so this dependency will not trigger anything to be installed unless a version
+# restriction is specified.
+REQUIRED_PACKAGES = [
+    'openbb',
+    'sendgrid==6.2.1',
+    'pandas_datareader',
+    'vaderSentiment',
+    'numpy',
+    'bs4',
+    'lxml',
+    'pandas_datareader',
+    'beautifulsoup4==4.10.0',
+    'xlrd==2.0.1',
+    'openpyxl==3.0.10'
+    ]
+
+
+setuptools.setup(
+    name='modules',
+    version='0.0.1',
+    description='Shres Runner Package.',
+    install_requires=REQUIRED_PACKAGES,
+    packages=setuptools.find_packages()
+    )
