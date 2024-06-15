bdd5e1c9f1047358021eeb241a82b84bff007b5e
mmistroni <mmistroni@gmail.com>
2024-06-15 19:23:41 +0100

    another try

--- dataflow/shareloader/dataflow_tester.py
+++ dataflow/shareloader/dataflow_tester.py
@@ -1,5 +1,5 @@
-from mypackage import dftester
+from mypackage import launcher
 import logging
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
-  dftester.run()
+  launcher.run()
