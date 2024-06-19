30a9db37a1d18cf459d32810f978ffcf0b20c849
mmistroni <mmistroni@gmail.com>
2024-06-19 07:11:19 +0100

    trying out new finviz

--- dataflow/shareloader/mypackage/obb_utils.py
+++ dataflow/shareloader/mypackage/obb_utils.py
@@ -1,6 +1,6 @@
 import logging
 import apache_beam as beam
-
+from openbb import obb
 import requests
 
 
@@ -11,36 +11,36 @@ class OBBLoader(beam.DoFn):
 
 
     def process(self, elements):
-        logging.info(f'Logging in to OBB.. pat={self.key}')
-        from openbb import obb
-        logging.info('activating obb')
-        obb.account.login(pat=self.pat)
+        try:
+            logging.info('activating obb')
+            obb.account.login(pat=self.pat)
+            logging.info('OBB ctivated obb')
 
-        results = []
-        for ticker in elements.split(','):
-            logging.info(f'Processing :{ticker}')
-            try:
-                dataDict = {}
-                income_statement = requests.get(
-                    'https://financialmodelingprep.com/api/v3/income-statement/{ticker}?limit=10&apikey={key}'.format(
-                        ticker=ticker, key=self.key)).json()
-                all_eps = [d['eps'] for d in income_statement]
+            results = []
+            for ticker in elements.split(','):
+                logging.info(f'Processing :{ticker}')
+                try:
+                    dataDict = {}
+                    income_statement = requests.get(
+                        'https://financialmodelingprep.com/api/v3/income-statement/{ticker}?limit=10&apikey={key}'.format(
+                            ticker=ticker, key=self.key)).json()
+                    all_eps = [d['eps'] for d in income_statement]
 
-                if len(all_eps) >= 6:
+                    if len(all_eps) >= 6:
 
-                    if all_eps[4] > 0:
-                        dataDict['epsGrowth5yrs'] = (all_eps[0] - all_eps[4]) / all_eps[4]
+                        if all_eps[4] > 0:
+                            dataDict['epsGrowth5yrs'] = (all_eps[0] - all_eps[4]) / all_eps[4]
 
-                    positive_eps = [e > 0 for e in all_eps]
-                    dataDict['positiveEps'] = len(positive_eps)
-                    dataDict['positiveEpsLast5Yrs'] = len([e > 0 for e in all_eps[0:5]])
-                    latest = income_statement[0]
-                    dataDict['netIncome'] = latest['netIncome']
-                    dataDict['income_statement_date'] = latest['date']
-                    dataDict['Ticker'] = ticker
-                    results.append(dataDict)
-            except Exception as e:
-                logging.info(f'Exception processin {ticker}:{str(e)}')
+                        positive_eps = [e > 0 for e in all_eps]
+                        dataDict['positiveEps'] = len(positive_eps)
+                        dataDict['positiveEpsLast5Yrs'] = len([e > 0 for e in all_eps[0:5]])
+                        latest = income_statement[0]
+                        dataDict['netIncome'] = latest['netIncome']
+                        dataDict['income_statement_date'] = latest['date']
+                        dataDict['Ticker'] = ticker
+                        results.append(dataDict)
+                except Exception as e:
+                    logging.info(f'Exception processin {ticker}:{str(e)}')
 
         return results
 
