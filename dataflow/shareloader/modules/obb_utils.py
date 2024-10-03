ced45bfd7409e411275bcfd56768224c0bbf42c7
mmistroni <mmistroni@gmail.com>
2024-10-03 08:00:22 +0100

    adding yfinance fetcher

--- dataflow/shareloader/modules/obb_utils.py
+++ dataflow/shareloader/modules/obb_utils.py
@@ -8,7 +8,7 @@ from pandas.tseries.offsets import BDay
 import asyncio
 
 def create_bigquery_ppln(p):
-    plus500_sql = """SELECT TICKER  FROM `datascience-projects.gcp_shareloader.plus500"""
+    plus500_sql = """SELECT TICKER  FROM `datascience-projects.gcp_shareloader.plus500'"""
     logging.info('executing SQL :{}'.format(plus500_sql))
     return (p | 'Reading-plus500}' >> beam.io.Read(
         beam.io.BigQuerySource(query=plus500_sql, use_standard_sql=True))
@@ -33,7 +33,7 @@ class AsyncProcess(beam.DoFn):
                                                             self.start_date.day,
                                                             9, 0, 0)]
         if filtered:
-            return filtered[-1:]
+            return filtered
         else:
             return []
 
