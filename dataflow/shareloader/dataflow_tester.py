1bdb641f6997174a1431f4eded2d980008dca239
mmistroni <mmistroni@gmail.com>
2024-10-05 22:01:28 +0100

    Update dataflow_tester.py

--- dataflow/shareloader/dataflow_tester.py
+++ dataflow/shareloader/dataflow_tester.py
@@ -1,6 +1,7 @@
 import logging
 from shareloader.modules import launcher
+import sys
 
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
-  launcher.run()
+  launcher.run(sys.argv)
