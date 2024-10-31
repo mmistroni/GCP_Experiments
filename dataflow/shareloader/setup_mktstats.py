70589d61d0d4d2cca61b484ccf2c441ee00f3288
mmistroni <mmistroni@gmail.com>
2024-10-31 07:27:50 +0000

    new attempt

--- dataflow/shareloader/setup_mktstats.py
+++ dataflow/shareloader/setup_mktstats.py
@@ -1,19 +1,30 @@
-from setuptools import setup, find_packages
 
+import setuptools
 
+# Configure the required packages and scripts to install.
+# Note that the Python Dataflow containers come with numpy already installed
+# so this dependency will not trigger anything to be installed unless a version
+# restriction is specified.
 REQUIRED_PACKAGES = [
     "apache-beam[gcp]",  # Must match the version in `Dockerfile``.
+    'sendgrid',
+    'pandas_datareader',
+    'vaderSentiment',
+    'numpy',
+    'bs4',
+    'lxml',
+    'pandas_datareader',
+    'beautifulsoup4',
+    'xlrd',
+    'openpyxl',
+    'openbb-yfinance'
     ]
 
 
-setup(
-    name="mypackage",  # Replace with your desired package name
-    version="0.1.0",  # Replace with your desired version
-    description="A short description of your package",
-    author="Your Name",
-    author_email="your_email@example.com",
+setuptools.setup(
+    name='shareloader',
+    version='0.0.1',
+    description='Shres Runner Package.',
     install_requires=REQUIRED_PACKAGES,
-    packages=find_packages()
-
-
-)
+    packages=setuptools.find_packages()
+    )
