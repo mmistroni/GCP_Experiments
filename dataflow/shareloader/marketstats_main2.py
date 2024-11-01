f51a86da13ec9e7836af0ab55c5fd3563821c776
mmistroni <mmistroni@gmail.com>
2024-11-01 14:33:45 +0000

    new attempt

--- dataflow/shareloader/marketstats_main2.py
+++ dataflow/shareloader/marketstats_main2.py
@@ -1,5 +1,5 @@
-from mypackage import launcher
+from shareloader.modules import launcher2
 import logging
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
-  launcher.run()
+  launcher2.run()
