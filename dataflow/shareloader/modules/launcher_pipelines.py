548049ea7fd473649863584b32e04da70d8604cd
mmistroni <mmistroni@gmail.com>
2025-03-12 07:24:26 +0000

    added company profile

--- dataflow/shareloader/modules/launcher_pipelines.py
+++ dataflow/shareloader/modules/launcher_pipelines.py
@@ -75,7 +75,7 @@ class StockSelectionCombineFn(beam.CombineFn):
   def add_input(self, accumulator, input):
     ROW_TEMPLATE = f"""<tr>
                           <td><b>{input.get('highlight', '')}</b></td>
-                          <td>{input['ticker']}({input.get('sector', '')}</td>
+                          <td>{input['ticker']}({input.get('sector', '')})</td>
                           <td>{input['prev_date']}</td>
                           <td>{input['prev_close']}</td>
                           <td>{input['date']}</td>
