0a9de5070498b573200c5647dfa6e1e7a1784dd0
mmistroni <mmistroni@gmail.com>
2025-06-26 16:45:01 +0100

    amending tolerance for eod fixing enod fday pips

--- dataflow/shareloader/modules/launcher_pipelines.py
+++ dataflow/shareloader/modules/launcher_pipelines.py
@@ -60,10 +60,10 @@ def run_etoro_pipeline(p, fmpkey, tolerance=0.08):
                 | 'ETORO LEAPSMaping extra ticker' >> beam.Map(lambda d: d['Ticker'])
                 | 'Filtering extra' >> beam.Filter(lambda tick: tick is not None and '.' not in tick and '-' not in tick)
                 | 'Combine all extratickers' >> beam.CombineGlobally(lambda x: ','.join(x))
-               | 'Etoro' >> beam.ParDo(AsyncProcess({'key':fmpkey}, cob, price_change=0.08, selection='EToro'))
+               | 'Etoro' >> beam.ParDo(AsyncProcess({'key':fmpkey}, cob, price_change=tolerance, selection='EToro'))
              )
 
-def run_newhigh_pipeline(p, fmpkey, tolerance=0.05):
+def run_newhigh_pipeline(p, fmpkey, tolerance=0.01):#even 1% will be godod for ne w hight
     cob = date.today()
     return  (p  | 'Starting nh' >> beam.Create(get_new_highs())
                 | 'nh Watchlist' >> beam.Map(lambda d: d['Ticker'])
@@ -73,13 +73,13 @@ def run_newhigh_pipeline(p, fmpkey, tolerance=0.05):
              )
 
 
-def run_extra_pipeline(p, fmpkey, tolerance=0.1):
+def run_extra_pipeline(p, fmpkey, tolerance=0.04):
     cob = date.today()
     return  (p  | 'Starting extras' >> beam.Create(get_extra_watchlist())
                 | 'Extra Watchlist' >> beam.Map(lambda d: d['Ticker'])
                 | 'Filtering extras ' >> beam.Filter(lambda tick: tick is not None and '.' not in tick and '-' not in tick)
                 | 'Combine all tickers from Extraextratickers' >> beam.CombineGlobally(lambda x: ','.join(x))
-               | 'Extras' >> beam.ParDo(AsyncProcess({'key':fmpkey}, cob, price_change=0.04, selection='ExtraWatch'))
+               | 'Extras' >> beam.ParDo(AsyncProcess({'key':fmpkey}, cob, price_change=tolerance, selection='ExtraWatch'))
              )
 
 
