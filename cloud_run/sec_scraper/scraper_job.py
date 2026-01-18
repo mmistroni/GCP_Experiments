bc52ee75a1ee26f512b67fa33a500924aa7179fe
mmistroni <mmistroni@gmail.com>
2026-01-18 16:13:04 +0000

    ss

--- cloud_run/sec_scraper/scraper_job.py
+++ cloud_run/sec_scraper/scraper_job.py
@@ -1,13 +1,14 @@
 import os
 import sys
 # Import the actual logic from your existing scraper.py
-from scraper import run_main_scraper 
+from scraper import run_master_scraper 
+import logging
 
 if __name__ == "__main__":
     # Get the instructions from the Cloud Run Job environment
     year_env = os.getenv("YEAR")
     qtr_env = os.getenv("QTR")
-
+    logging.info(f'Running for {year_env}/{qtr_env}')
     if not year_env or not qtr_env:
         print("❌ CRITICAL ERROR: YEAR or QTR environment variables not found.")
         sys.exit(1)
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     try:
         # Just call your real scraper function
         # It will use the Table ID already defined in scraper.py
-        run_main_scraper(year=int(year_env), qtr=int(qtr_env))
+        run_master_scraper(year=int(year_env), qtr=int(qtr_env))
     except Exception as e:
         print(f"❌ SCRAPER CRASHED: {str(e)}")
         sys.exit(1)
